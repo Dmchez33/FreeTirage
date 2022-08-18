e7166b4053fb26f0b6cbcdddba88ba9282fa63ab
Dmchez33 <iddembele206@gmail.com>
2022-08-18 13:26:57 +0000

    creation du projet

---
+++ src/main/java/com/idrissabarema/apifreetirage/ApiFreeTirageApplication.java
@@ -0,0 +1,13 @@
+package com.idrissabarema.apifreetirage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFreeTirageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFreeTirageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/idrissabarema/apifreetirage/ApiFreeTirageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idrissabarema.apifreetirage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFreeTirageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
